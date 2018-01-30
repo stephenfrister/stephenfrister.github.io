7fcd690e967546a1f48772f9bdd443dbab78f1c3
sgf <stephen.frister@hotmail.com>
2018-01-29 23:16:15 -0800

    Basic Character Information
    Displays basic user information for single characters.

--- scripts/clockwork.js
+++ scripts/clockwork.js
@@ -9,6 +9,7 @@ firebase.initializeApp(config);
 
 var ping = firebase.database().ref('_ping');
 var users = firebase.database().ref('users');
+//var ghosts = firebase.database().ref('ghosts');
 
 function pingServer()
 {
@@ -16,100 +17,128 @@ function pingServer()
 		return (current_value || 0) + 1; 
 	});
 }	
+
+function getPing(divID)
+{
+	var obj = document.getElementById(divID);
+		 
+    ping.on("value", function(snapshot) {
+        obj.innerHTML = snapshot.val();
+        });
+
+}
+
+function claimExp(userId1, userId2)
+{
+    var userId = userId1.toString().concat( userId2.toString() );
+    //var userId = userId1.concat(userId2);
+    //userId = ""
+    //userId = 
+    var exp = firebase.database().ref('users/' + userId + '/exp' );
+    //var exp = firebase.database().ref('users/108360043117969116770/exp' );
+	exp.transaction(function (current_value) {
+		return (current_value || 0) + 1; 
+	});
+}	
+
 
 function getUsers(divID)
 {
 	var obj = document.getElementById(divID);
-	
-	if(type == "clockworkUsersDiv")
-	{				 
-		totalGob.on("users", function(snapshot) {
-			obj.innerHTML = snapshot.val();
-			});
-	}
+    
+    obj.innerHTML = ""
+    
+    var chars = new Array(); 
+    var lvl = new Array();    
+    var hps = new Array(); 
+    var exp = new Array();  
+    
+    users.orderByValue().on("value", function(characters) {
+    
+        var html = ""
+        var i = 0; 
+    
+        html = "<table id='table2'>"
+        
+        characters.forEach(function(userid) {
+            html += "<tr>"
+            //obj.innerHTML += "<div>" + "user: " + userid.key + "</div>";
+            
+            userid.forEach(function(attribute) {
+                if(attribute.key == "cname") {
+                    chars[i] = attribute.val();
+                    //chars[i] = userid.key
+                }
+                if(attribute.key == "exp") {
+                    exp[i] = attribute.val();
+                }
+                else if(attribute.key == "health") {
+                    hps[i] = attribute.val();
+                }
+                else if(attribute.key == "level") {
+                    lvl[i] = attribute.val();
+                }
+                
+                else if( attribute.key == "power" || attribute.key == "magic" ) {
+                    // empty for now
+                }
+                else {
+                    // empty ? 
+                }
+                
+            });
+            
+            if(typeof chars[i] == 'undefined'){
+                chars[i] = "???"
+            }            
+            if(typeof lvl[i] == 'undefined'){
+                lvl[i] = "?"
+            }            
+            if(typeof hps[i] == 'undefined'){
+                hps[i] = "?"
+            }            
+            if(typeof exp[i] == 'undefined'){
+                exp[i] = "?"
+            }            
+            html += "<td style='width:150px'>"
+            html += chars[i]
+            html += "</td>"
+            
+            html += "<td style='width:50px'>"
+            html += lvl[i]
+            html += "</td>"
+            
+            html += "<td style='width:50px'>"
+            html += hps[i]
+            html += "</td>"
+            
+            html += "<td style='width:100px'>"
+            html += exp[i]
+            html += "</td>"  
+            
+            //var userId = "108360043117969116770"
+            var userId = userid.key
+            
+            userId1 = userId.slice(0,10)
+            userId2 = userId.slice(10,userId.length)
+            
+            html += "<td style='width:150px'>"
+            html += "<button type='button' "
+            html += "onclick='claimExp("
+            html += userId1 + "," + userId2
+            html += ")'>Claim Daily Exp</button></td></td>"
+            
+            html += "</tr>"
+            i++;      
+            
+        });        
+        
+        html += "</table>"        
+        obj.innerHTML = html
+        
+    });
+    
 }
-
-/*
-
-var ref = firebase.database().ref('node/clicks');
-ref.transaction(function(currentClicks) {
-  // If node/clicks has never been set, currentRank will be `null`.
-  return (currentClicks || 0) + 1;
-});
-
-var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-starCountRef.on('value', function(snapshot) {
-  updateStarCount(postElement, snapshot.val());
-});
-
-var userId = firebase.auth().currentUser.uid;
-return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-  var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-  // ...
-});
-*/
-
-
-/*
-function pingServer()
-{
-	ping.transaction(function (current_value) {
-		return (current_value || 0) + 1; 
-	});
-}	
-*/
-
-
-/*
-function foobar(divID)
-{
-	var obj = document.getElementById(divID);
-	
-	var HTML = ""
-	
-	obj.innerHTML = "";
-	
-	lists.on("value", function(snapshot) {
-		//obj.innerHTML += "<div>" + snapshot.key() + "</div>";
-		//obj.innerHTML += "<table style='width:100%'>"
-		
-		HTML = "<table>";
-		
-		snapshot.forEach(function(listKeys) {
-			//obj.innerHTML += "<div>" + listKeys.key() + "</div>";
-			HTML += "<tr>";
-			HTML += "<td>" + listKeys.child("listName").val() + "</td>";
-			HTML += "<td>" + listKeys.child("owner").val() + "</td>";			
-			HTML += "</tr>";
-		});
-		HTML += "</table>"
-		
-		obj.innerHTML = HTML; 
-		//obj.innerHTML += "</table>"
-	});
-	
-	//obj.innerHTML = HTML; 
-	
-	//lists.orderByValue().on("value", function(snapshot) {
-	// lists.once("value", function(snapshot) {
-		// //obj.innerHTML += snapshot.val();
-		// //obj.innerHTML += snapshot.key();
-		// snapshot.forEach(function(childSnapshot) {
-			// obj.innerHTML += "<div> foo: " + childSnapshot.key() + "</div>"; 
-		// )};
-	// });	
-
-}
-*/
-
-/*
-function foo()
-{
-	root.transaction(function (current_value) {
-		return (current_value || 0) + 1; 
-	});
-}	
-*/
 
 
 
