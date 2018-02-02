450af96fc5d06e0d306c56fc3b8122e89e633915
sgf <stephen.frister@hotmail.com>
2018-02-02 01:01:37 -0800

    UI Improvements
    Relatively clean UI that incorporates Users, Titles, and Story
    Description.

--- scripts/clockwork.js
+++ scripts/clockwork.js
@@ -9,8 +9,10 @@ firebase.initializeApp(config);
 
 var ping = firebase.database().ref('_ping');
 var users = firebase.database().ref('users');
+
+
 //var ghosts = firebase.database().ref('ghosts');
-//var ghosts = firebase.database().ref('chapters');
+
 
 function pingServer()
 {
@@ -25,7 +27,7 @@ function getPing(divID)
 		 
     ping.on("value", function(snapshot) {
         obj.innerHTML = snapshot.val();
-        });
+    });
 
 }
 
@@ -40,11 +42,161 @@ function claimExp(userId1, userId2)
 }	
 
 
+
+var chapters = firebase.database().ref('chapters');
+var chapterAct = firebase.database().ref('chapters/active');
+var chapterPart = firebase.database().ref('chapters/activepart');
+
+
+function getTitleAct(divID)
+{
+    var html = ""
+    //var obj = document.getElementById(divID);  
+    chapterAct.on("value", function(snapshot) {
+         //obj.innerHTML = snapshot.val();
+         html = setTitleAct( divID, snapshot.val() )
+    });
+    
+    var obj = document.getElementById(divID);
+    obj.innerHTML = html;
+    
+    
+}
+
+function setTitleAct( divID, chapter )
+{  
+    
+    //var chapters = firebase.database().ref('chapters');
+    //var mvar = firebase.database().ref('chapters/' + chapter + "/title")
+    
+    //var html = ""
+    //return firebase.database().ref('/users/' + userId).once('value').then( 
+    //return firebase.database().ref('chapters/' + chapter + "/title").once('value').then( 
+        //function(snapshot) {
+            //html = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        //}
+    //);
+    
+    //var obj = document.getElementById(divID);
+    //obj.innerHTML = html;
+         
+}
+
+/*
+return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+// ...
+});
+
+var userId = firebase.auth().currentUser.uid;
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+         
+mvar.on("value", function(snapshot) {
+     obj.innerHTML = snapshot.val();
+});
+*/
+
+
+
+//var mvar = firebase.database().ref('chapters/' + 0 + "" + 1 + "")
+
+function getTitlePart(divID)
+{
+    var obj = document.getElementById(divID);  
+    chapterPart.on("value", function(snapshot) {
+         obj.innerHTML = snapshot.val();
+    });
+}
+
+function getStoryParts(divId1, divId2, divId3)
+{
+    var obj1 = document.getElementById(divId1);  
+    var obj2 = document.getElementById(divId2); 
+    var obj3 = document.getElementById(divId3); 
+    
+    var act = "0"
+    var part = "0"
+    var description = "..."
+    
+    chapters.on("value", function(snapshot) {
+
+        chapterAct.once("value", function(snapshot) {
+             act = snapshot.val();
+        });
+
+        chapterPart.once("value", function(snapshot) {
+             part = snapshot.val();
+        });
+        
+        obj1.innerHTML = "Act " + act + ": " + snapshot.child("/" + act + "/title").val();
+        obj2.innerHTML = "Part " + part + ": " + snapshot.child("/" + act + "/" + part + "/title").val();
+        obj3.innerHTML = snapshot.child("/" + act + "/" + part + "/description").val();
+        
+    });
+    
+}
+
+    //var dynamicChapter = firebase.database().ref('chapters/' + act + "/" + part + "/description") 
+
+function getStory(divID)
+{
+    // var fb_act = chapters.database().ref('active/');
+    // var fb_act = chapters.database().ref('chapters/active');
+    //var fb_part = firebase.database().ref('chapters/activeparts');
+    
+	//var obj = document.getElementById(divID);
+    //obj.innerHTML = "";
+    
+    //chapters.on("value", function(snapshot) {
+    //     obj.innerHTML = snapshot.val();
+    //});
+    
+    var act = ""
+    var part = ""
+    var ctitle = ""
+    var ptitle = ""
+    var description = ""
+    
+    /*
+    fb_part.on('value', function(snapshot) {
+        part = snapshot.val();
+    });
+    
+    var fb_ctitle = firebase.database().ref('chapters/' + act + '/title');
+    var fb_ptitle = firebase.database().ref('chapters/' + act + '/' + '/' + part + '/title');
+    var fb_description = firebase.database().ref('chapters/' + act + '/' + '/' + part + '/description');
+    
+    fb_ctitle.on('value', function(snapshot) {
+        ctitle = snapshot.val();
+    });
+    fb_ptitle.on('value', function(snapshot) {
+        ptitle = snapshot.val();
+    });
+    fb_description.on('value', function(snapshot) {
+        description = snapshot.val();
+    });
+    */
+    
+    
+    var html = "";
+    
+    html = "<div>"
+    //html += "Test"
+    html += act
+    html += part
+    html += ctitle
+    html += ptitle
+    html += description
+    html += "</div>"
+    
+    //obj.innerHTML = html;
+    
+}
+
 function getUsers(divID)
 {
 	var obj = document.getElementById(divID);
     
-    obj.innerHTML = ""
+    obj.innerHTML = "";
     
     var chars = new Array(); 
     var lvl = new Array();    
@@ -54,6 +206,7 @@ function getUsers(divID)
     users.orderByValue().on("value", function(characters) {
     
         var html = ""
+        var dropdowns = ""
         var i = 0; 
     
         html = "<table id='users-table'>"
@@ -68,7 +221,6 @@ function getUsers(divID)
         
         characters.forEach(function(userid) {
             html += "<tr>"
-            //obj.innerHTML += "<div>" + "user: " + userid.key + "</div>";
                 
             userid.forEach(function(attribute) {
                 
@@ -96,13 +248,6 @@ function getUsers(divID)
             });
             
             if(typeof chars[i] != 'undefined') {
-                // skip
-            
-                /*
-                if(typeof chars[i] == 'undefined'){
-                    chars[i] = "???"
-                } 
-                */
                 
                 if(typeof lvl[i] == 'undefined'){
                     lvl[i] = "?"
@@ -113,22 +258,18 @@ function getUsers(divID)
                 if(typeof exp[i] == 'undefined'){
                     exp[i] = "?"
                 }            
-                //html += "<td style='width:150px'>"
                 html += "<td>"
                 html += chars[i]
                 html += "</td>"
                 
-               // html += "<td style='width:50px'>"
                 html += "<td>"
                 html += lvl[i]
                 html += "</td>"
                 
-                //html += "<td style='width:120px' align='center'>"
                 html += "<td align='center'>"
                 html += hps[i] + " / " + hps[i]
                 html += "</td>"
                 
-                //html += "<td style='width:100px' align='center'>"
                 html += "<td align='center'>"
                 html += exp[i]
                 html += "</td>"  
@@ -138,12 +279,14 @@ function getUsers(divID)
                 userId1 = userId.slice(0,10)
                 userId2 = userId.slice(10,userId.length)
                 
-                //html += "<td style='width:150px' align='center'>"
                 html += "<td align='center'>"
-                html += "<button type='button' class='ff7' "
-                html += "onclick='claimExp("
+                html += "<button "
+                html += "type='action-button' "
+                html += "class='ff7 dropbtn' "
+                html += "onclick='actionClick("
                 html += userId1 + "," + userId2
-                html += ")'>Action</button></td></td>"
+                html += ")' >"
+                html += "Action</button></td></td>"
                 
                 html += "</tr>"   
             }
@@ -151,13 +294,54 @@ function getUsers(divID)
             
         });        
         
-        html += "</table>"        
+        html += "</table>"  
+        //html += dropdowns 
+        
         obj.innerHTML = html
         
     });
     
 }
 
+
+function actionClick(userId1, userId2)
+{
+    document.getElementById("actionMenu").classList.toggle("show");
+    
+    var mString = "onclick=claimExp(" + userId1 + "," + userId2 + ")"
+    document.getElementById("actionMenuExp").setAttribute("onclick", mString);
+    
+    document.getElementById("actionMenuAttack").style.color = "#33334d";
+    document.getElementById("actionMenuMagic").style.color = "#33334d";
+    document.getElementById("actionMenuItem").style.color = "#33334d";
+    
+    document.getElementById("actionMenuAttack").className = "disabled-hover";
+    document.getElementById("actionMenuMagic").className = "disabled-hover";
+    document.getElementById("actionMenuItem").className = "disabled-hover";
+    
+    //document.getElementById("actionMenuAttack").setAttribute("onclick", mString);
+    //document.getElementById("actionMenuMagic").setAttribute("onclick", mString);
+    //document.getElementById("actionMenuItem").setAttribute("onclick", mString);
+    
+    //document.getElementById("actionMenu").innerHTML = "whatever";
+    //document.getElementById("actionMenu").setAttribute("id", "div_top1");
+    
+}	
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("actionmenu-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
 
