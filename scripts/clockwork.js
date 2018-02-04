98c8edcf9d030aa6983c3c3537171a676767b28a
sgf <stephen.frister@hotmail.com>
2018-02-04 02:10:31 -0800

    Updates
    More granular character data.

--- scripts/clockwork.js
+++ scripts/clockwork.js
@@ -9,9 +9,11 @@ firebase.initializeApp(config);
 
 var ping = firebase.database().ref('_ping');
 var users = firebase.database().ref('users');
-
-
 //var ghosts = firebase.database().ref('ghosts');
+
+var chapters = firebase.database().ref('chapters');
+var chapterAct = firebase.database().ref('chapters/active');
+var chapterPart = firebase.database().ref('chapters/activepart');
 
 
 function pingServer()
@@ -35,77 +37,20 @@ function claimExp(userId1, userId2)
 {
     var userId = userId1.toString().concat( userId2.toString() );
     var exp = firebase.database().ref('users/' + userId + '/exp' );
-	exp.transaction(function (current_value) {
-		return (current_value || 0) + 1; 
+    var hps = firebase.database().ref('users/' + userId + '/health' );
+    
+	hps.transaction(function (current_value) {
+    
+        if(current_value > 0) {
+            exp.transaction(function (current_value) {
+                return (current_value || 0) + 1; 
+            });            
+            return (current_value || 0) - 1;
+        }        
+        
 	});
     
 }	
-
-
-
-var chapters = firebase.database().ref('chapters');
-var chapterAct = firebase.database().ref('chapters/active');
-var chapterPart = firebase.database().ref('chapters/activepart');
-
-
-function getTitleAct(divID)
-{
-    var html = ""
-    //var obj = document.getElementById(divID);  
-    chapterAct.on("value", function(snapshot) {
-         //obj.innerHTML = snapshot.val();
-         html = setTitleAct( divID, snapshot.val() )
-    });
-    
-    var obj = document.getElementById(divID);
-    obj.innerHTML = html;
-    
-    
-}
-
-function setTitleAct( divID, chapter )
-{  
-    
-    //var chapters = firebase.database().ref('chapters');
-    //var mvar = firebase.database().ref('chapters/' + chapter + "/title")
-    
-    //var html = ""
-    //return firebase.database().ref('/users/' + userId).once('value').then( 
-    //return firebase.database().ref('chapters/' + chapter + "/title").once('value').then( 
-        //function(snapshot) {
-            //html = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-        //}
-    //);
-    
-    //var obj = document.getElementById(divID);
-    //obj.innerHTML = html;
-         
-}
-
-/*
-return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-// ...
-});
-
-var userId = firebase.auth().currentUser.uid;
-    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-         
-mvar.on("value", function(snapshot) {
-     obj.innerHTML = snapshot.val();
-});
-*/
-
-
-
-//var mvar = firebase.database().ref('chapters/' + 0 + "" + 1 + "")
-
-function getTitlePart(divID)
-{
-    var obj = document.getElementById(divID);  
-    chapterPart.on("value", function(snapshot) {
-         obj.innerHTML = snapshot.val();
-    });
-}
 
 function getStoryParts(divId1, divId2, divId3)
 {
@@ -135,63 +80,6 @@ function getStoryParts(divId1, divId2, divId3)
     
 }
 
-    //var dynamicChapter = firebase.database().ref('chapters/' + act + "/" + part + "/description") 
-
-function getStory(divID)
-{
-    // var fb_act = chapters.database().ref('active/');
-    // var fb_act = chapters.database().ref('chapters/active');
-    //var fb_part = firebase.database().ref('chapters/activeparts');
-    
-	//var obj = document.getElementById(divID);
-    //obj.innerHTML = "";
-    
-    //chapters.on("value", function(snapshot) {
-    //     obj.innerHTML = snapshot.val();
-    //});
-    
-    var act = ""
-    var part = ""
-    var ctitle = ""
-    var ptitle = ""
-    var description = ""
-    
-    /*
-    fb_part.on('value', function(snapshot) {
-        part = snapshot.val();
-    });
-    
-    var fb_ctitle = firebase.database().ref('chapters/' + act + '/title');
-    var fb_ptitle = firebase.database().ref('chapters/' + act + '/' + '/' + part + '/title');
-    var fb_description = firebase.database().ref('chapters/' + act + '/' + '/' + part + '/description');
-    
-    fb_ctitle.on('value', function(snapshot) {
-        ctitle = snapshot.val();
-    });
-    fb_ptitle.on('value', function(snapshot) {
-        ptitle = snapshot.val();
-    });
-    fb_description.on('value', function(snapshot) {
-        description = snapshot.val();
-    });
-    */
-    
-    
-    var html = "";
-    
-    html = "<div>"
-    //html += "Test"
-    html += act
-    html += part
-    html += ctitle
-    html += ptitle
-    html += description
-    html += "</div>"
-    
-    //obj.innerHTML = html;
-    
-}
-
 function getUsers(divID)
 {
 	var obj = document.getElementById(divID);
@@ -205,9 +93,15 @@ function getUsers(divID)
     
     users.orderByValue().on("value", function(characters) {
     
-        var html = ""
-        var dropdowns = ""
-        var i = 0; 
+        var i = 0;
+        var html = "";
+        var dropdowns = "";
+        
+        chars   = [];
+        lvl     = []; 
+        exp     = [];   
+        hps     = [];  
+        hmax    = []; 
     
         html = "<table id='users-table'>"
         
@@ -220,7 +114,6 @@ function getUsers(divID)
         html += "</tr>"
         
         characters.forEach(function(userid) {
-            html += "<tr>"
                 
             userid.forEach(function(attribute) {
                 
@@ -234,10 +127,12 @@ function getUsers(divID)
                 else if(attribute.key == "health") {
                     hps[i] = attribute.val();
                 }
+                else if(attribute.key == "healthmax") {
+                    hmax[i] = attribute.val();
+                }
                 else if(attribute.key == "level") {
                     lvl[i] = attribute.val();
                 }
-                
                 else if( attribute.key == "power" || attribute.key == "magic" ) {
                     // empty for now
                 }
@@ -249,12 +144,17 @@ function getUsers(divID)
             
             if(typeof chars[i] != 'undefined') {
                 
+                html += "<tr>"
+                
                 if(typeof lvl[i] == 'undefined'){
                     lvl[i] = "?"
                 }            
                 if(typeof hps[i] == 'undefined'){
                     hps[i] = "?"
-                }            
+                }             
+                if(typeof hps[i] == 'undefined'){
+                    hmax[i] = "?"
+                }           
                 if(typeof exp[i] == 'undefined'){
                     exp[i] = "?"
                 }            
@@ -267,7 +167,7 @@ function getUsers(divID)
                 html += "</td>"
                 
                 html += "<td align='center'>"
-                html += hps[i] + " / " + hps[i]
+                html += hps[i] + " / " + hmax[i]
                 html += "</td>"
                 
                 html += "<td align='center'>"
@@ -289,16 +189,14 @@ function getUsers(divID)
                 html += "Action</button></td></td>"
                 
                 html += "</tr>"   
+                
+                html += "</tr>"
             }
             i++;   
             
         });        
-        
         html += "</table>"  
-        //html += dropdowns 
-        
         obj.innerHTML = html
-        
     });
     
 }
@@ -342,30 +240,6 @@ window.onclick = function(event) {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
