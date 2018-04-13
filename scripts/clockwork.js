94473799a48da3c2c2e524630cf7a8a42a2ed8e9
Stephen Frister <stephen.frister@hotmail.com>
2018-04-12 23:55:51 -0700

    Changed Story Scroll
    Added smooth scrolling to story div.

--- scripts/clockwork.js
+++ scripts/clockwork.js
@@ -253,8 +253,107 @@ function getUsers(divID)
     });
 }
 
-function scrollDiv(){
-    document.getElementById("story-div").scrollTop += 200;
+function setStoryScrollListener(){
+    
+    document.getElementById('story-div').addEventListener('scroll', function () {
+            
+        sh = document.getElementById("story-div").scrollHeight;
+        st = document.getElementById("story-div").scrollTop;
+        oh = document.getElementById("story-div").offsetHeight; 
+        
+        if ( sh - st < oh ) {
+            document.getElementById("id-arrow").classList.add("noshow");
+        }
+            
+    }, false);
+    
+}
+
+//function scrollDiv(){
+    
+    //mScrollOld = document.getElementById("story-div").scrollTop
+    
+    //mBorder = 7; 
+    //mHeight = document.getElementById("story-div").offsetHeight - mBorder; 
+    //document.getElementById("story-div").scrollTop += mHeight;
+    
+    //mScrollNew = document.getElementById("story-div").scrollTop
+    
+    //if ( mScrollNew == mScrollOld ) {
+    //    document.getElementById("id-arrow").classList.toggle("noshow");
+    //}
+    //else if (mScrollNew - mScrollOld < mHeight ) {
+    //    document.getElementById("id-arrow").classList.toggle("noshow");
+    //}
+    
+//}
+
+
+// http://jsfiddle.net/0uwg96sh/
+function scroll_down() {
+    var el = document.getElementById("story-div");
+    smooth_scroll_to(el, el.scrollTop + 200, 600);
+}
+var smooth_scroll_to = function(element, target, duration) {
+    target = Math.round(target);
+    duration = Math.round(duration);
+    if (duration < 0) {
+        return Promise.reject("bad duration");
+    }
+    if (duration === 0) {
+        element.scrollTop = target;
+        return Promise.resolve();
+    }
+    var start_time = Date.now();
+    var end_time = start_time + duration;
+    var start_top = element.scrollTop;
+    var distance = target - start_top;
+    
+    // based on http://en.wikipedia.org/wiki/Smoothstep
+    var smooth_step = function(start, end, point) {
+        if(point <= start) { return 0; }
+        if(point >= end) { return 1; }
+        var x = (point - start) / (end - start); // interpolation
+        return x*x*(3 - 2*x);
+    }
+    return new Promise(function(resolve, reject) {
+        // This is to keep track of where the element's scrollTop is
+        // supposed to be, based on what we're doing
+        var previous_top = element.scrollTop;
+
+        // This is like a think function from a game loop
+        var scroll_frame = function() {
+            if(element.scrollTop != previous_top) {
+                reject("interrupted");
+                return;
+            }
+            // set the scrollTop for this frame
+            var now = Date.now();
+            var point = smooth_step(start_time, end_time, now);
+            var frameTop = Math.round(start_top + (distance * point));
+            element.scrollTop = frameTop;
+
+            // check if we're done!
+            if(now >= end_time) {
+                resolve();
+                return;
+            }
+            // If we were supposed to scroll but didn't, then we
+            // probably hit the limit, so consider it done; not
+            // interrupted.
+            if(element.scrollTop === previous_top
+                && element.scrollTop !== frameTop) {
+                resolve();
+                return;
+            }
+            previous_top = element.scrollTop;
+
+            // schedule next frame for execution
+            setTimeout(scroll_frame, 0);
+        }
+        // boostrap the animation process
+        setTimeout(scroll_frame, 0);
+    });
 }
 
 function actionClick(userId1, userId2){
