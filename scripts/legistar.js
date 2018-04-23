5041af060f68b0b7b9ad5eb986388218c8c7a375
Stephen Frister <stephen.frister@hotmail.com>
2018-04-22 21:22:29 -0700

    Comment Cleanup
    - removed some comments

--- scripts/legistar.js
+++ scripts/legistar.js
@@ -10,40 +10,30 @@ var config = {
 
 firebase.initializeApp(config);
 
-//var index = firebase.database().ref('index');
-
 function searchSubmit(){
     
     // http://webapi.legistar.com/v1/Seattle/matters/1796/Texts/1897
-    // http://webapi.legistar.com/v1/Seattle/matters/<?1>/Texts/<?2>
     // http://webapi.legistar.com/v1/Seattle/matters/<matterId>/Texts/<textId>
+    
     // legistar:    index/search/<key>/<value>
     // matterId:    <value>
     // textId:      <key>
-    // http://webapi.legistar.com/v1/Seattle/matters/<value>/Texts/<key>
     
-    //console.log( "submit... " + i )
-    //i++; 
+    // http://webapi.legistar.com/v1/Seattle/matters/<value>/Texts/<key>
     
     value = ""; 
     value = document.getElementById('id-search-field').innerHTML;
     
     document.getElementById('id-results').innerHTML = "";
     
-    //var searchTerm = 'police'; 
     var searchTerm = value;
     var searchIndex = 'index/' +  searchTerm;
     
     console.log("searchIndex: " + searchIndex);
-
-    //console.log("here.. " + window.innerWidth);
     
     var search = firebase.database().ref(searchIndex);
     search.once('value').then(function(snapshot) {
         var data = snapshot.val();
-        
-        //console.log( "index... " + i )
-        //console.log( "data: " + data )
         
         rValue = []; 
         
@@ -57,30 +47,17 @@ function searchSubmit(){
                 mValue = data[key];
                 mLink = "http://webapi.legistar.com/v1/Seattle/matters/" + mValue + "/Texts/" + mKey;
                 
-                //console.log(mLink);
-                
-                //rValue += "Link: " + mLink + "<br>";
-                //rValue.push("Link: " + mLink + "<br>");
                 
                 mTemp = "<div>MatterId: <b>" + mValue + "</b>, TextId: <b>" + mKey + "</b>:";
                 //mTemp += "&nbsp;&nbsp;<a href=" + mLink + ">" + mLink + "</a><div>";
                 
-                
-
-                var test = window.innerWidth
-                console.log("test.. " + test );
-                
-                //if (true) {
                 if (parseInt(test) < parseInt(700) ) {
-                    console.log("less.. " );
                     mTemp += "<br>&nbsp;&nbsp;<a href=" + mLink + ">" + mLink + "</a><div>";
                 }
                 else {
-                    console.log("greater.. " );
                     mTemp += "&nbsp;&nbsp;<a href=" + mLink + ">" + mLink + "</a><div>";
                 }
                 /**/
-                
                 
                 rValue.push( mTemp );
                 
@@ -89,29 +66,12 @@ function searchSubmit(){
             });
             
             rValue.reverse(); 
-            //rValue = rValue.join("<br>"); 
             rValue = rValue.join(" "); 
             
-            //<a href="url">link text</a>
-            
-            //mResults = ""; 
-            //for mLink in rValue {
-                
-                //mResults += "<div><a href=" +  + ">" + ... + "</a><div>"
-                //mResults += "<a href=" + mLink + ">" + mLink + "</a>"
-                
-                //rValue = rValue.join(" "); 
-
-            //}            
-            
-            
-            //document.getElementById('id-results').innerHTML += "Link: " + mLink + "<br>";
             document.getElementById('id-results').innerHTML += rValue;
         
         }
-        
     });
-    /* */
     
 }
 
